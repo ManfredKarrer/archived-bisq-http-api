89960967f554c931cd8c7df6544e40f45d0ad34d
Manfred Karrer <mk@nucleo.io>
2018-05-31 02:30:10 +0200

    Add files from https://github.com/mrosseel/bisq-api

---
+++ src/main/java/network/bisq/api/model/PaymentAccountList.java
@@ -0,0 +1,13 @@
+package network.bisq.api.model;
+
+import java.util.List;
+
+
+
+import network.bisq.api.model.payment.PaymentAccount;
+
+public class PaymentAccountList {
+
+    public List<PaymentAccount> paymentAccounts;
+
+}
---
+++ src/main/java/network/bisq/api/service/v1/ClosedTradableResource.java
@@ -0,0 +1,36 @@
+package network.bisq.api.service.v1;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import network.bisq.api.BisqProxy;
+import network.bisq.api.model.ClosedTradableList;
+
+@Slf4j
+@Api(value = "closed-tradables", authorizations = @Authorization(value = "accessToken"))
+@Produces(MediaType.APPLICATION_JSON)
+public class ClosedTradableResource {
+
+    private final BisqProxy bisqProxy;
+
+    public ClosedTradableResource(BisqProxy bisqProxy) {
+        this.bisqProxy = bisqProxy;
+    }
+
+    @ApiOperation("List portfolio history")
+    @GET
+    public ClosedTradableList listClosedTrades() {
+        final ClosedTradableList list = new ClosedTradableList();
+        list.closedTradables = bisqProxy.getClosedTradableList();
+        list.total = list.closedTradables.size();
+        return list;
+    }
+
+}
